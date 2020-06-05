c0b74d7f32f7776ca223c70cf0c5fb10afd38504
PRAMOD A JINGADE <avj2352@gmail.com>
2020-06-05 18:54:57 +0800

    small changes

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import ThemeConfigProvider from "../components/theme-config/ThemeConfigProvider";
 import { IAppContextState, useGlobalState } from "../common/context/AppContext";
 
-function App() {
+const App: FunctionComponent = () => {
     const appContext: IAppContextState = useGlobalState();
-  return (
-    <ThemeConfigProvider theme={appContext.theme}>
-        <h1>
-            Welcome to Tailwind v{appContext.version}
-        </h1>
-    </ThemeConfigProvider>
-  );
-}
+    return (
+        <ThemeConfigProvider theme={appContext.theme}>
+            <h1>
+                Welcome to Tailwind v{appContext.version}
+            </h1>
+        </ThemeConfigProvider>
+    );
+};
 
 export default App;
