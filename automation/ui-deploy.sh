37ac3ccffb6742240c711527fa07c30bea005179
PRAMOD A JINGADE <avj2352@gmail.com>
2020-12-26 16:36:13 +0800

    updated build scripts

--- automation/ui-deploy.sh
+++ automation/ui-deploy.sh
@@ -1,5 +1,5 @@
 # Script to only copy over build folder
 echo "Deploying UI - from Source folder to Heroku project...."
-cp -a ../node-workspace/i-am-bored/frontend/build just-bored/build
+cp -a ../node-workspace/i-am-bored/frontend/build/ just-bored/build
 echo "Deleting Source location build folder...."
 rm -rf ../node-workspace/i-am-bored/frontend/build
